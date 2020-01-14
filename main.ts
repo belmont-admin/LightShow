a39123aea66d254380897542fc835c8c787d7f5b
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-14 20:50:21 +0000

    Update README.md, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "red") {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else if (receivedString == "amber") {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    } else if (receivedString == "green") {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    channel += 1
+    basic.showNumber(channel)
+    radio.setGroup(channel)
+})
+input.onButtonPressed(Button.A, function () {
+    channel += -1
+    basic.showNumber(channel)
+    radio.setGroup(channel)
+})
+let channel = 0
+channel = 1
+basic.showNumber(channel)
+radio.setGroup(1)
