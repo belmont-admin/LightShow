8caa28cd0d339183ad74f1d3c37af9f0fe69a839
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-28 13:47:17 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ radio.onReceivedString(function (receivedString) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(1000)
         pins.digitalWritePin(DigitalPin.P0, 0)
-    } else if (receivedString == "amber") {
+    } else if (receivedString == "yellow") {
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.pause(1000)
         pins.digitalWritePin(DigitalPin.P1, 0)
