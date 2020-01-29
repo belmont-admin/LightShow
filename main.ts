dc5ad7900e5e59248ff391911bbad3a522a3331d
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-29 15:58:12 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,32 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "red") {
         pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P0, 0)
     } else if (receivedString == "yellow") {
         pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P1, 0)
     } else if (receivedString == "green") {
         pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P2, 0)
     }
+    basic.pause(delay)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
 input.onButtonPressed(Button.B, function () {
     channel += 1
     basic.showNumber(channel)
     radio.setGroup(channel)
 })
+radio.onReceivedNumber(function (receivedNumber) {
+    delay = receivedNumber
+})
 input.onButtonPressed(Button.A, function () {
     channel += -1
     basic.showNumber(channel)
     radio.setGroup(channel)
 })
+let delay = 0
 let channel = 0
 channel = 1
+delay = 1000
 basic.showNumber(channel)
 radio.setGroup(1)
